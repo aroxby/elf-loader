7522a0f905481825cb41a6dcbb7be80eea1fa6cd
Andy Roxby <aroxby@users.noreply.github.com>
2022-06-20 11:18:25 -0400

    Clean up loader.h

--- src/loader.h
+++ src/loader.h
@@ -1,7 +1,11 @@
-#include "elf64.h"
+#ifndef __INC_LOADER_H_
+#define __INC_LOADER_H_
+
 #include <string>
 
 class ElfLoader {
 public:
     ElfLoader(const std::string &path);
 };
+
+#endif//__INC_LOADER_H_
