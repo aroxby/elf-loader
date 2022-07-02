de7bb4fba890e6ea3e6601ccc275414308f4de36
Andy Roxby <aroxby@users.noreply.github.com>
2022-07-02 05:01:57 -0400

    Load and dump symbols

--- src/elf_image.cpp
+++ src/elf_image.cpp
@@ -6,6 +6,14 @@
 #include "elf_image.h"
 using namespace std;
 
+template <typename ValType, typename RangeType>
+bool _inRange(ValType val, RangeType lower, RangeType upper) {
+    if(val <= lower || val >= upper) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 ElfImage::ElfImage(istream &is) {
     // Read the header
@@ -34,16 +42,22 @@ ElfImage::ElfImage(istream &is) {
         is.read((char*)&section_headers[i], sizeof(Elf64_Shdr));
     }
 
-    // Load string section
-    strings = unique_ptr<char[]>(new char[section_headers[elf_header.e_shstrndx].sh_size]);
-    is.seekg(section_headers[elf_header.e_shstrndx].sh_offset);
-    is.read(&strings[0], section_headers[elf_header.e_shstrndx].sh_size);
-
     // Load program headers
     is.seekg(elf_header.e_phoff);
     program_headers = unique_ptr<Elf64_Phdr[]>(new Elf64_Phdr[elf_header.e_phnum]);
     for(int i = 0; i < elf_header.e_phnum; i++) {
         is.read((char*)&program_headers[i], sizeof(Elf64_Phdr));
+    }
+
+    // Load section header string table by known index
+    section_strings = loadStringTable(section_headers[elf_header.e_shstrndx], is);
+
+    // Selectively load section data
+    for(int i = 0; i < elf_header.e_shnum; i++) {
+        switch(section_headers[i].sh_type) {
+        case SHT_SYMTAB:
+            loadSymbolTable(section_headers[i], is);
+        }
     }
 
     allocateMemory();
@@ -55,6 +69,29 @@ ElfImage::ElfImage(istream &is) {
             loadSegment(program_headers[i], is);
         }
     }
+}
+
+unique_ptr<char[]> ElfImage::loadStringTable(const Elf64_Shdr &header, istream &is) {
+    auto strings = unique_ptr<char[]>(new char[header.sh_size]);
+    is.seekg(header.sh_offset);
+    is.read(&strings[0], header.sh_size);
+    return strings;
+}
+
+void ElfImage::loadSymbolTable(const Elf64_Shdr &header, istream &is) {
+    // TODO: throw if we already loaded symbols
+
+    if(header.sh_size % sizeof(Elf64_Sym) != 0) {
+        // TODO: Are we okay with reusing this exception type?
+        throw UnsupportedFileConfiguration();
+    }
+
+    num_symbols = header.sh_size / sizeof(Elf64_Sym);
+    symbols = unique_ptr<Elf64_Sym[]>(new Elf64_Sym[num_symbols]);
+    is.seekg(header.sh_offset);
+    is.read((char*)&symbols[0], header.sh_size);
+
+    symbol_strings = loadStringTable(section_headers[header.sh_link], is);
 }
 
 void ElfImage::allocateMemory() {
@@ -104,7 +141,7 @@ void ElfImage::dump(ostream &os) {
     for(int i = 0; i < elf_header.e_shnum; i++) {
         os << endl;
         os << "Section Name Offset: " << section_headers[i].sh_name << endl;
-        os << "Section Name: " << &strings[section_headers[i].sh_name] << endl;
+        os << "Section Name: " << &section_strings[section_headers[i].sh_name] << endl;
         os << "Section Type: " << (void*)((size_t)section_headers[i].sh_type)
             << " (" << sectionTypeToString(section_headers[i].sh_type) << ')' << endl;
         os << "Section Type Name: " << getSectionTypeName(section_headers[i].sh_type) << endl;
@@ -115,7 +152,7 @@ void ElfImage::dump(ostream &os) {
         os << "Section Size: " << section_headers[i].sh_size << endl;
         os << "Related Section: "
             << section_headers[i].sh_link << " ("
-            << &strings[section_headers[section_headers[i].sh_link].sh_name]
+            << &section_strings[section_headers[section_headers[i].sh_link].sh_name]
             << ')' << endl;
         os << "Section Info: " << (void*)((size_t)section_headers[i].sh_info) << endl;
         os << "Section Alignment: " << (void*)section_headers[i].sh_addralign << endl;
@@ -136,5 +173,23 @@ void ElfImage::dump(ostream &os) {
         os << "Segment File Size: " << program_headers[i].p_filesz << endl;
         os << "Segment Memory Size: " << program_headers[i].p_memsz << endl;
         os << "Segment Alignment: " << (void*)program_headers[i].p_align << endl;
+    }
+
+    // Dump symbols
+    for(int i = 0; i < num_symbols; i++) {
+        auto section_index_for_name = _inRange(
+            symbols[i].st_shndx, SHN_LORESERVE, SHN_HIRESERVE
+        ) ? 0 : symbols[i].st_shndx;
+        os << endl;
+        os << "Symbol Name Offset: " << symbols[i].st_name << endl;
+        os << "Symbol Name: " << &symbol_strings[symbols[i].st_name] << endl;
+        os << "Symbol Bind: " << ELF64_ST_BIND(symbols[i].st_info) << endl;
+        os << "Symbol Type: " << ELF64_ST_TYPE(symbols[i].st_info) << endl;
+        os << "Symbol Visibility: " << ELF64_ST_VISIBILITY(symbols[i].st_other) << endl;
+        os << "Symbol Section Index: " << symbols[i].st_shndx << endl;
+        os << "Symbol Section Name: "
+            << &section_strings[section_headers[section_index_for_name].sh_name] << endl;
+        os << "Symbol Value: " << (void*)symbols[i].st_value << endl;
+        os << "Symbol Size: " << symbols[i].st_size << endl;
     }
 }
--- src/elf_image.h
+++ src/elf_image.h
@@ -13,13 +13,19 @@ public:
     void dump(std::ostream &os);
 
 private:
+    std::unique_ptr<char[]> loadStringTable(const Elf64_Shdr &header, std::istream &is);
+    void loadSymbolTable(const Elf64_Shdr &header, std::istream &is);
     void allocateMemory();
     void loadSegment(const Elf64_Phdr &header, std::istream &is);
 
     Elf64_Ehdr elf_header;
     std::unique_ptr<Elf64_Shdr[]> section_headers;
-    std::unique_ptr<char[]> strings;
     std::unique_ptr<Elf64_Phdr[]> program_headers;
+    std::unique_ptr<char[]> section_strings;
+
+    size_t num_symbols;
+    std::unique_ptr<Elf64_Sym[]> symbols;
+    std::unique_ptr<char[]> symbol_strings;
 
     std::unique_ptr<char[]> image_base;
 };
