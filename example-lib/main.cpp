dff47b74c4be6c55e0e6c705e2cd705926a69b26
Andy Roxby <aroxby@users.noreply.github.com>
2022-07-26 10:57:12 -0400

    Use a simplier export symbol name

--- example-lib/main.cpp
+++ example-lib/main.cpp
@@ -3,7 +3,7 @@
     #warning You are not targetting Unix.  Your complier will probably NOT generate an ELF binary.
 #endif
 
-int example_function() {
+extern "C" int example_function() {
     // printf is used here (instead of cout) because it will be easier to hack later
     return printf("This is the example lib!\n");
 }
