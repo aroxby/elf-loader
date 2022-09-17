a6145489dbb624838229df2de1357f75fd2a1feb
Andy Roxby <aroxby@users.noreply.github.com>
2022-09-17 11:12:36 -0400

    Install shims, lookup function, run

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include "elf_module.h"
 using namespace std;
 
+SYSV int printWrapper(const char *str) {
+    return printf("%s", str);
+}
 
 int main(int argc, char *argv[]) {
     if (argc!=3) {
@@ -14,7 +17,24 @@ int main(int argc, char *argv[]) {
     ifs.exceptions(ifstream::eofbit | ifstream::failbit | ifstream::badbit);
     ifs.open(argv[1], ios_base::in | ios_base::binary);
 
-    ElfModule library(ElfModule::DynamicShims(), ifs);
+    ElfModule::DynamicShims shims;
+
+    // I'm relatively sure these can all be null
+    // We could actually check the symbol table and see that they are weak
+    shims["__gmon_start__"] = nullptr;
+    shims["_ITM_registerTMCloneTable"] = nullptr;
+    shims["_ITM_deregisterTMCloneTable"] = nullptr;
+    shims["__cxa_finalize"] = nullptr;
+
+    shims["printf"] = (const void*)printWrapper;
+    ElfModule library(shims, ifs);
+
+    const void *example_function_addr = library.getSymbolAddress(argv[2]);
+
+    // FIXME: Messy syntax
+    SYSV int(*example_function)() = (SYSV int (*)())example_function_addr;
+    int r = example_function();
+    printf("Ran %i\n", r);
 
     return 0;
 }
